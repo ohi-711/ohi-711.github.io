521e9da514fcf74caf2c8cf83479afd346d84893
April Sun <aprilsun711@gmail.com>
2026-07-20 03:55:04 -0400

    added see more button to experiences

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function toggleMoreWork() {
     moreWork.forEach(work => work.classList.toggle('show'));
     btn.textContent = isHidden ? 'See more' : 'See less';
 }
+
+function toggleMoreExperience() {
+    const moreExperience = document.querySelectorAll('.more-experience');
+    const btn = document.getElementById('see-more-exp-btn');
+    const isHidden = moreExperience[0].classList.contains('show');
+
+    moreExperience.forEach(exp => exp.classList.toggle('show'));
+    btn.textContent = isHidden ? 'See more' : 'See less';
+}
